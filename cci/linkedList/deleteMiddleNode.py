edc55d62a94895d66badf16b819b8281ebe34eb9
Fei <fei-fei.zheng@ibm.com>
2021-01-13 10:29:07 -0500

    add deleteMiddleNode

--- cci/linkedList/deleteMiddleNode.py
+++ cci/linkedList/deleteMiddleNode.py
@@ -1,9 +1,17 @@
 from linkedList import ListNode, LinkedList
 
 # write a method delete any node but first and last node. nothing returned. 
-def deleteMiddleNode(head: ListNode, target: int):
-
+def deleteMiddleNode(node: ListNode):
+	node.val = node.next.val
+	node.next = node.next.next
 
 list1 = LinkedList()
-list1.insertByArray([3, 4, 5, 6, 1, 7, 3, 2, 6, 9, 1, 1])
+list1.insertByArray([3, 4, 5, 6, 1, 7])
+node = list1.head
+i = 3
+while i > 0:
+	node = node.next
+	i -= 1
 
+deleteMiddleNode(node)  # node.val = 6
+print(list1.toString())
